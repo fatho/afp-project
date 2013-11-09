0608e5a91a1f7b10c562f70162d77b47a4c5bcdf
Meike Grewing <meike@ce-tec.de>
2013-11-09 20:46:59 +0100

    Added new language and one superfluous bounds check.

--- GuessTheNumber/Handler/NewGame.hs
+++ GuessTheNumber/Handler/NewGame.hs
@@ -1,7 +1,6 @@
 module Handler.NewGame (getNewGameR, startNewGame) where
 
 import Import
-import Control.Monad
 import System.Random
 import Logic.State
 
@@ -11,12 +10,15 @@ getNewGameR = curry startNewGame
 -- | Creates the initial game state and redirects to guessing page.
 -- Make sure that the range argument is validated before calling this function.
 startNewGame :: (Int, Int) -> Handler Html
-startNewGame (lowerBound, upperBound) = do
-  -- Setup Game
-  num   <- liftIO $ getStdRandom $ randomR (lowerBound, upperBound)
-  state <- liftIO $ newSalt $ GameState
-                { myNumber = num  
-                , guessHistory = []
-                , salt = 0
-                }
-  redirect (GuessR $ encryptGameState state)
+startNewGame (lowerBound, upperBound) = 
+  if lowerBound > upperBound 
+    then redirect RulesR -- just to be sure...
+    else do
+      -- Setup Game
+      num   <- liftIO $ getStdRandom $ randomR (lowerBound, upperBound)
+      state <- liftIO $ newSalt $ GameState
+                    { myNumber = num  
+                    , guessHistory = []
+                    , salt = 0
+                    }
+      redirect (GuessR $ encryptGameState state)
