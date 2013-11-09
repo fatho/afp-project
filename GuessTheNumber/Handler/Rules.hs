079580320d1434999563dca5e6c23f44bbb9f623
Fabian Thorand <f.thorand@gmail.com>
2013-11-09 14:06:18 +0100

    Made rule form look nicer.

--- GuessTheNumber/Handler/Rules.hs
+++ GuessTheNumber/Handler/Rules.hs
@@ -2,24 +2,26 @@
 module Handler.Rules (getRulesR, postRulesR) where
 
 import Import
+import Foundation
 import Handler.NewGame
 
 getRulesR :: Handler Html
-getRulesR = do
-  (formWidget, formEnctype) <- generateFormPost ruleForm
-  defaultLayout $ do 
-    setTitle "Guess The Number"
-    $(widgetFile "rules")
+getRulesR = renderRulePage (1,100) Nothing
 
 postRulesR :: Handler Html
 postRulesR = do
-  ((result, formWidget), formEnctype) <- runFormPost ruleForm
-  case result of
-      FormSuccess range -> startNewGame range
-      _ -> defaultLayout $ do
-            $(widgetFile "rules")
+  range <- runInputPost $ (,)
+            <$> iopt intField "lower" 
+            <*> iopt intField "upper"
+  case range of
+    (Just lower, Just upper)
+      | lower <= upper -> startNewGame (lower, upper)
+      | otherwise      -> renderRulePage (lower,upper) $ Just MsgInvalidRange
+    (Just lower, _)    -> renderRulePage (lower, 100) $ Just MsgNoInput
+    (_, Just upper)    -> renderRulePage (1, upper) $ Just MsgNoInput
+    (_,_)              -> renderRulePage (1, 100) $ Just MsgNoInput
 
-ruleForm :: Form (Int, Int)
-ruleForm = renderDivs $ (,)
-              <$> areq intField "lower" (Just 1)
-              <*> areq intField "upper" (Just 100)
+renderRulePage :: (Int, Int) -> Maybe AppMessage -> Handler Html
+renderRulePage (lower, upper) formError = defaultLayout $ do
+  setTitle "Guess The Number"
+  $(widgetFile "rules")
